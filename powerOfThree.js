095fca157ef2d7737f8cae432480f381c4756e55
Samuel Akinboro <victorakinboro2003@gmail.com>
2022-08-24 11:51:33 +0100

    simpler solution

--- powerOfThree.js
+++ powerOfThree.js
@@ -4,26 +4,39 @@ Given an integer n, return true if it is a power of three. Otherwise, return fal
 An integer n is a power of three, if there exists an integer x such that n == 3x.
 */
 
-var isPowerOfThree = function(n) {
-  const shouldContinue = n % 3;
-  const dividedByThree = n / 3
+// var isPowerOfThree = function(n) {
+//   const shouldContinue = n % 3;
+//   const dividedByThree = n / 3
   
-  if (n !== 0) {
-    if (n === 1){
-      return true
-    }
-    if (shouldContinue === 0) {
-      if (dividedByThree < 1) return false
-      if (dividedByThree === 1) {
-        return true
-      }
-      if(dividedByThree > 1){
-        return isPowerOfThree(dividedByThree)
-      }
-    }
-  }
+//   if (n !== 0) {
+//     if (n === 1){
+//       return true
+//     }
+//     if (shouldContinue === 0) {
+//       if (dividedByThree < 1) return false
+//       if (dividedByThree === 1) {
+//         return true
+//       }
+//       if(dividedByThree > 1){
+//         return isPowerOfThree(dividedByThree)
+//       }
+//     }
+//   }
   
-  return false
-};
+//   return false
+// };
 
-console.log('final->', isPowerOfThree(45));
+
+// simpler approach
+const isPowerOfThree = (n) => {
+  if (n < 1) return false
+
+  while(n % 3 === 0){
+    n /= 3
+  }
+
+  return n === 1;
+}
+
+
+console.log('final->', isPowerOfThree(27));
